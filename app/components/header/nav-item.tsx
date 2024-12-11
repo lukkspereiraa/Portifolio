01979de23b3ffae07ade45bbab91804bc54bb207
lukkspereiraa <lukkspereiraa@gmail.com>
2024-12-11 08:59:46 -0300

    ADD : componet header

--- app/components/header/nav-item.tsx
+++ app/components/header/nav-item.tsx
@@ -1,5 +1,7 @@
+"use client"
+import { cn } from "@/app/lib/utils"
 import Link from "next/link"
-import { type } from "os"
+import {usePathname} from "next/navigation"
 
 type NaveItemProps = {
     label: string,
@@ -7,8 +9,17 @@ type NaveItemProps = {
 }
 
 export const NavItem = ({label, href}: NaveItemProps) =>{
+
+    const pathName = usePathname();
+    const isActive = pathName === href;
+
+    
+
     return(
-        <Link href={href}>
+        <Link className={cn(
+            "text-gray-400 flex items-center font-mono font-medium",
+            isActive && 'text-gray-50'
+        )} href={href}>
             <samp className="text-emerald-400" >#</samp>
             {label}
         </Link>
