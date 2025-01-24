6b059aaea50ab10edb6b198a7200c5f7eb39a924
lukkspereiraa <lukkspereiraa@gmail.com>
2025-01-23 21:39:24 -0300

    chore: editando o linke do projects-list

--- app/components/pages/projects/project-list/index.tsx
+++ app/components/pages/projects/project-list/index.tsx
@@ -5,7 +5,7 @@ const ProjectList = () => {
     return ( 
     <section className="container py-32 grid grid-cols-1 sm:grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-x-4
     gap-y-6">
-        <Link href="/projects">
+        <Link href="/projects/test">
             <ProjectCard />
         </Link>
     </section> );
